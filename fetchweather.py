24b11ac8ccd45c03c9d705dc9d3fe8a873cedd3e
Sam Stifter <sam.stifter@gmail.com>
2018-09-11 18:57:25 -0500

    Update dosumentation on fetchweather

--- fetchweather.py
+++ fetchweather.py
@@ -6,10 +6,11 @@ from requests import RequestException
 def get_current_forecast(location):
     """Get current forecast data.
 
-    Given a latitude and longitude (e.g. "42.025,-93.646") get the
-    current weather forecast from Weather.gov API
-    Returns: A description of the current weather forecast or
-    "Error" if error occurs
+    Given a latitude and longitude, fetches the current weather forecast from Weather.gov API
+    Args:
+        location (str): a latitude and longitude point seperated by a comma (e.g. 42.02,-93.65)
+    Returns:
+        A description of the current weather forecast or "Error" if error occurs
     """
     # Download the JSON data for the Point from Weather.gov API
     url = 'https://api.weather.gov/points/%s' % location
