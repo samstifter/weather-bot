a907195a0f0068249d301d7ea0b97c38fc63caf7
Sam Stifter <sam.stifter@gmail.com>
2018-09-11 19:21:23 -0500

    Add main and init to use weather and mail together

---
+++ main.py
@@ -0,0 +1,29 @@
+import configparser
+from fetchweather import *
+from mail import *
+
+
+def main():
+    """
+    Gets weather and sends it in an email
+
+    This function uses fetchweather and mail to send an email based on the email settings
+    and location in the weatherbot.ini file
+    """
+    # Get configuration file
+    config = configparser.ConfigParser()
+    config.read('weatherbot.ini')
+
+    # Get weather info
+    data = get_current_forecast(config['location']['coordinates'])
+
+    # Send an email if the info was fetched successfully
+    if data != "Error":
+        print(data)
+        send_mail('sam.stifter@gmail.com', "Weather", data)
+
+
+main()
+
+
+
